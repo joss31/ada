30f0e0394cccf2e8300a61847ec0597e84528f75
joss31 <joseline.zevallos@ucsp.edu.pe>
2016-05-25 14:22:34 -0500

    Cut Rod tiempos

--- cutRod.cpp
+++ cutRod.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <time.h>
+#include <random>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
-// esta funcion es muy ineficiente ya que tarda mucho tiempo en ejecutarse, 
+
+// esta funcion es muy ineficiente ya que tarda mucho tiempo en ejecutarse,
 //tambien por su llamada recursiva se llama varias veces los mismos parametros
 //por eso es su tiempo de ejecucion es 2ˆ(n-1).
-
 int cutRod(int* p, int n){
     if(n==0){
         return 0;
@@ -18,7 +22,6 @@ int cutRod(int* p, int n){
 
 //Aqui se utiliza una programacion dinamica, y tambien se llama a recursividad
 //y eso hace que no sea tan eficiente aunque es mejor que la funcion cutRod.
-
 int memoized_cutRod_aux(int* p, int n, int* r){
     if(n!=0 and r[n-1]>=0){
         return r[n-1];
@@ -43,12 +46,12 @@ int memoized_cutRod(int* p, int n){
         r[i]=-9999;
     }
     return memoized_cutRod_aux(p,n,r);
+
 }
 
 // Aqui el tiempo de ejecucion es de nˆ2, y es el mas eficiente que
 //los otros dos anteriores ya que en la parte del bucle hace menos operaciones
 //que en las otras funciones que se llama hasta la recursividad.
-
 int bottom_up_cutRod(int* p, int n){
     int* r=new int [n+1];
     r[0]=0;
@@ -64,21 +67,31 @@ int bottom_up_cutRod(int* p, int n){
 
 int main()
 {
-  int * a=new int[10];
-  a[0]=1;
-  a[1]=5;
-  a[2]=8;
-  a[3]=9;
-  a[4]=10;
-  a[5]=17;
-  a[6]=17;
-  a[7]=20;
-  a[8]=24;
-  a[9]=30;
-  int var_size=8;
-  cout<<cutRod(a,var_size)<<endl;
-  cout<<memoized_cutRod(a,var_size)<<endl;
-  cout<<bottom_up_cutRod(a,var_size)<<endl;
-  delete a;
-  return 0;
+    int var_size;
+    cin>>var_size;
+    int * a=new int[var_size];
+    for(unsigned int i=0;i<var_size;++i)
+        a[i]=10*i+rand()%10;
+    cout<<"start"<<endl;
+    clock_t start, end1;
+    double joss_time;
+        start = clock();
+    //El cut rod esta comentado porque al momento de hacer las pruebas demora mucho por eso es muy ineficiente.
+    //cutRod(a,var_size);
+        end1 = clock();
+        joss_time=end1-start;
+        cout<<"cutRod: "<<joss_time/CLOCKS_PER_SEC<<endl;
+        start = clock();
+    memoized_cutRod(a,var_size);
+        end1 = clock();
+        joss_time=end1-start;
+        cout<<"memorized cutRod: "<<joss_time/CLOCKS_PER_SEC<<endl;
+        start = clock();
+    bottom_up_cutRod(a,var_size);
+        end1 = clock();
+        joss_time=end1-start;
+        cout<<"bottom cutRod: "<<joss_time/CLOCKS_PER_SEC<<endl;
+    delete a;
+    system("pause");
+    return 0;
 }
